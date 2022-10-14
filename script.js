0f36c6a20f091fda7eb6f2ce0d3d6c67183f1ff6
Simon <simonrojas.dev@gmail.com>
2022-10-14 19:51:51 -0300

    Mejora validacion

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
 const form = document.querySelector("#contacto");
-let msjEnvio = document.querySelector("#msjEnvio");
+const msjEnvio = document.querySelector("#msjEnvio");
+const buttonSubmit = document.querySelector("#submit");
 
 form.addEventListener("submit", (event)=>{
+    let error = false;
     Array.from(document.querySelectorAll(".validar")).forEach(elemento =>{
-        error = false;
-        if(elemento.id === "campBot"){
-            if(elemento.value !== ""){
+        if(elemento.id === "inputEmail"){
+            if((/^(([^<>()\[\]\.,;:\s@\”]+(\.[^<>()\[\]\.,;:\s@\”]+)*)|(\”.+\”))@(([^<>()[\]\.,;:\s@\”]+\.)+[^<>()[\]\.,;:\s@\”]{2,})$/).test(elemento.value) == false){
+                msjEnvio.style.color = "red";
+                msjEnvio.innerHTML = "Por favor Ingrese un email valido.";
                 error = true;
             }
         }
-        else if(elemento.id === "inputEmail"){
-            if((/^(([^<>()\[\]\.,;:\s@\”]+(\.[^<>()\[\]\.,;:\s@\”]+)*)|(\”.+\”))@(([^<>()[\]\.,;:\s@\”]+\.)+[^<>()[\]\.,;:\s@\”]{2,})$/).test(elemento.value) == false){
+        else if(elemento.id === "campBot"){
+            if(elemento.value !== ""){
+                msjEnvio.style.color = "red";
+                msjEnvio.innerHTML = "Mmmmm, eres un BOT.";
                 error = true;
             }
         }
         else if(elemento.value == "" || elemento.value == "-None-"){
+            msjEnvio.style.color = "red";
+            msjEnvio.innerHTML = "Por favor Ingrese todos los campos.";
                 error = true;
         }
     })
-    if(error === true){
+    if(error == true){
         event.preventDefault();
     }else{
-        msjEnvio.innerHTML = "Gracias, su mensaje fue enviado correctamente.";
+        msjEnvio.style.color = "#84b6f4";
+        msjEnvio.innerHTML = "Gracias, su mensaje fue enviado correctamente."
     }
-})
+});
+
+buttonSubmit.addEventListener("click", function(){
+    
+});
+
